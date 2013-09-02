3e1bffd9f967985508b7df4a35d17dc791164665
Georgiana Serea <georgyserea@gmail.com>
2013-09-02 08:21:08 +0200

    ordenado de carpetas

---
+++ usuario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+   	<meta charset="utf-8">
+   	<title>Formulario de acceso </title>
+   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	</head>
+	<body>
+	<?php	
+		$usuario=$_POST['usuario']
+		echo 	" <h1>Buenos dias $usuario. </h1> " ;	
+		echo 	" <p> Tu contrase&ntilde;a es: $_POST[contrasena] </p> " ;
+	?>
+	</body> 
+</html >
